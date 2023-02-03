cfcda9367ebdffb01e958f5a30b64f482b6b1f9a
unknown <bakerbret425@gmail.com>
2023-02-02 23:22:47 -0800

    updates

--- server.js
+++ server.js
@@ -1,4 +1,30 @@
-const inquirer = require('inquirer')
-const mysql = require('mysql2')
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+const cTable = require('console.table');
 
-require('dotenv').config()
+require('dotenv').config();
+
+const connection = mysql.createConnection({
+    host:'127.0.0.1',
+    user:'root',
+    password:'password',
+    database:'employee_db'
+});
+
+connection.connect(err => {
+    if (err) throw err;
+    console.log('connected as id ' + connection.threadId);
+    afterConnection();
+  });
+
+  
+// function after connection is established and welcome image shows 
+afterConnection = () => {
+    console.log("***********************************")
+    console.log("*                                 *")
+    console.log("*        EMPLOYEE MANAGER         *")
+    console.log("*                                 *")
+    console.log("***********************************")
+    promptUser();
+  };
+  
